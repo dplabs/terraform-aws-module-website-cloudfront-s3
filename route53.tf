b224a1d79c7659b1ff9ad7d4e26db0e772c9e929
Daniel Pecos Martinez <me@danielpecos.com>
2023-10-18 13:13:28 +0200

    refactor: website redirections in Route53 pointing directly to the S3 website URL - no Cloudfront needed

--- route53.tf
+++ route53.tf
@@ -2,31 +2,30 @@ data "aws_route53_zone" "main" {
   name = var.route53_zone_name
 }
 
-resource "aws_route53_record" "root-a" {
+resource "aws_route53_record" "root-a-cloudfront" {
+  count = var.target_domain == null ? 1 : 0
+
   zone_id = data.aws_route53_zone.main.zone_id
   name    = module.website_s3_bucket.s3_bucket
   type    = "A"
 
   alias {
-    name                   = var.target_domain == null ? aws_cloudfront_distribution.s3_distribution[0].domain_name : aws_cloudfront_distribution.s3_distribution_redirection[0].domain_name
-    zone_id                = var.target_domain == null ? aws_cloudfront_distribution.s3_distribution[0].hosted_zone_id : aws_cloudfront_distribution.s3_distribution_redirection[0].hosted_zone_id
+    name                   = aws_cloudfront_distribution.s3_distribution[0].domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution[0].hosted_zone_id
     evaluate_target_health = false
   }
 }
 
-resource "aws_route53_record" "validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.default.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+resource "aws_route53_record" "root-a-s3-website-redirection" {
+  count = var.target_domain == null ? 0 : 1
 
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.main.zone_id
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = module.website_s3_bucket.s3_bucket
+  type    = "A"
+
+  alias {
+    name                   = module.website_s3_bucket.s3_bucket_website_endpoint
+    zone_id                = module.website_s3_bucket.s3_bucket_hosted_zone_id
+    evaluate_target_health = false
+  }
 }
