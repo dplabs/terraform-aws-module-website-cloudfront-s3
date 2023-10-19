786d97118534c797214fb7c046864ec0eee49720
Daniel Pecos Martinez <me@danielpecos.com>
2023-10-19 10:34:46 +0200

    refactor: s3 terraform module renamed

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "website_s3_bucket" {
-  source = "github.com/dplabs/terraform-module-private-s3-bucket"
-  #source = "../terraform-module-private-s3-bucket"
+  source = "github.com/dplabs/terraform-module-s3-bucket"
+  #source = "../terraform-module-s3-bucket"
 
   domain_name = var.domain
   redirect_to = var.target_domain
